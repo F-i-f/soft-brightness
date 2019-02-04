63c49cc93bde0aa1b7f558ba264620d0199b76cd
Philippe Troin <phil+github-commits@fifi.org>
2019-02-04 05:00:44 +0000

    Complete rework
    Now completely replaces to indicator.

--- extension.js
+++ extension.js
@@ -14,31 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+const Lang = imports.lang;
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Mainloop = imports.mainloop;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
 const Convenience = Me.imports.convenience;
-const Brightness = imports.ui.main.panel.statusArea.aggregateMenu._brightness;
-const BrightnessSlider = Brightness._slider;
+const Indicator = imports.ui.status.brightness.Indicator;
+const AggregateMenu = imports.ui.main.panel.statusArea.aggregateMenu;
 
 let debug = null;
 let enabled = null;
 let settings = null;
-let overlays = null;
 let debugSettingChangedConnection = null;
-let minBrightnessSettingChangedConnection = null;
-let currentBrightnessSettingChangedConnection = null;
-let useBacklightSettingChangedConnection = null;
-let monitorsSettingChangedConnection = null;
-let monitorsChangedConnection = null;
-let brightnessIndicatorOriginalSync = null;
-let sliderChangedBacklightConnection = null;
-let sliderChangedBacklightCode = null;
-let sliderChangedConnection = null;
+let modifiedIndicator = null;
 
 function log(what) {
     global.log('Soft-Brightness: '+what);
@@ -50,133 +41,209 @@ function log_debug(what) {
     }
 }
 
-function hideOverlays() {
-    if (overlays != null) {
-	log_debug("drop overlays");
-	for (let i=0; i < overlays.length; ++i) {
-	    Main.uiGroup.remove_actor(overlays[i]);
-	}
-	overlays = null;
-    }
-}
+let ModifiedIndicator = new Lang.Class({
+    Name: 'ModifiedBrightnessIndicator',
+    Extends: Indicator,
 
-function showOverlays(opacity) {
-    if (overlays == null) {
-	let monitorConfig = settings.get_string('monitors');
-	let monitors;
-	log_debug('showOverlays(): monitorConfig='+monitorConfig);
-	if (monitorConfig == "All") {
-	    monitors = Main.layoutManager.monitors;
-	} else if (monitorConfig == "Built-in") {
-	    monitors = [Main.layoutManager.primaryMonitor];
-	} else if (monitorConfig == "External") {
-	    monitors = [];
-	    for (let i=0; i < Main.layoutManager.monitors.length; ++i) {
-		if (Main.layoutManager.monitors[i] != Main.layoutManager.primaryMonitor) {
-		    monitors.push(Main.layoutManager.monitors[i]);
-		}
+    _init() {
+	this.parent();
+
+	this._overlays = null;
+
+	this._monitorsChangedConnection = null;
+	this._minBrightnessSettingChangedConnection = null;
+	this._currentBrightnessSettingChangedConnection = null;
+	this._monitorsSettingChangedConnection = null;
+	this._useBacklightSettingChangedConnection = null;
+    },
+
+    _swapMenu(oldIndicator, newIndicator) {
+	let menuItems = AggregateMenu.menu._getMenuItems();
+	let menuIndex = null;
+	for (let i = 0; i < menuItems.length; i++) {
+	    if (oldIndicator.menu == menuItems[i]) {
+		menuIndex = i;
+		break;
 	    }
-	} else {
-	    log("showOverlays(): Unhandled \"monitors\" setting = "+monitorConfig);
+	}
+	if (menuIndex == null) {
+	    log('_swapMenu(): Cannot find brightness indicator');
+	    return false;
+	}
+	log_debug('_swapMenu(): Replacing brightness menu item at index '+menuIndex);
+	menuItems.splice(menuIndex, 1);
+	oldIndicator._proxy.run_dispose();
+	oldIndicator.menu.destroy();
+	AggregateMenu.menu.addMenuItem(newIndicator.menu, menuIndex);
+	AggregateMenu._brightness = newIndicator;
+	return true;
+    },
+
+    _enable() {
+	log_debug('_enable()');
+
+	if (! this._swapMenu(AggregateMenu._brightness, this)) {
 	    return;
 	}
-	overlays = [];
-	for (let i=0; i < monitors.length; ++i) {
-	    let monitor = monitors[i];
-	    log_debug('Create overlay #'+i+': '+monitor.width+'x'+monitor.height+'@'+monitor.x+','+monitor.y);
-	    let overlay = new St.Label({
-		style_class: 'brightness-overlay',
-		text: "",
-	    });
-	    overlay.set_position(monitor.x, monitor.y);
-	    overlay.set_width(monitor.width);
-	    overlay.set_height(monitor.height);
-	    overlay.set_z_position(0.00001);
-	    Main.uiGroup.add_actor(overlay);
-	    overlays.push(overlay);
+
+	this._monitorsChangedConnection = Main.layoutManager.connect('monitors-changed', this._on_monitors_change.bind(this));
+	this._minBrightnessSettingChangedConnection = settings.connect('changed::min-brightness', this._on_brightness_change.bind(this));
+	this._currentBrightnessSettingChangedConnection = settings.connect('changed::current-brightness', this._on_brightness_change.bind(this));
+	this._monitorsSettingChangedConnection = settings.connect('changed::monitors', this._on_monitors_change.bind(this));
+	this._useBacklightSettingChangedConnection = settings.connect('changed::use-backlight', this._on_use_backlight_change.bind(this));
+
+	// If we use the backlight and the Brightness proxy is null, it's still connecting and we'll get a _sync later.
+	if (! settings.get_boolean('use-backlight') || this._proxy.Brightness != null) {
+	    let curBrightness = this._getBrightnessLevel();
+	    this._sliderChanged(this._slider, curBrightness);
+	    this._slider.setValue(curBrightness);
+	}
+    },
+
+    _disable() {
+	log_debug('_disable()');
+
+	let standardIndicator = new imports.ui.status.brightness.Indicator();
+	this._swapMenu(this, standardIndicator);
+
+	Main.layoutManager.disconnect(this._monitorsChangedConnection);
+	settings.disconnect(this._minBrightnessSettingChangedConnection);
+	settings.disconnect(this._currentBrightnessSettingChangedConnection);
+	settings.disconnect(this._monitorsSettingChangedConnection);
+	settings.disconnect(this._useBacklightSettingChangedConnection);
+	this._hideOverlays();
+    },
+
+    _sliderChanged(slider, value) {
+	log_debug("_sliderChanged(slide, "+value+")");
+	this._storeBrightnessLevel(value);
+    },
+
+    _sync() {
+	log_debug("_sync()");
+	this._on_brightness_change();
+	this._slider.setValue(this._getBrightnessLevel());
+    },
+
+    _hideOverlays() {
+	if (this._overlays != null) {
+	    log_debug("drop overlays, count="+this._overlays.length);
+	    for (let i=0; i < this._overlays.length; ++i) {
+		Main.uiGroup.remove_actor(this._overlays[i]);
+	    }
+	    this._overlays = null;
+	}
+    },
+
+    _showOverlays(opacity) {
+	if (this._overlays == null) {
+	    let monitorConfig = settings.get_string('monitors');
+	    let monitors;
+	    log_debug('_showOverlays(): monitorConfig='+monitorConfig);
+	    if (monitorConfig == "All") {
+		monitors = Main.layoutManager.monitors;
+	    } else if (monitorConfig == "Built-in") {
+		monitors = [Main.layoutManager.primaryMonitor];
+	    } else if (monitorConfig == "External") {
+		monitors = [];
+		for (let i=0; i < Main.layoutManager.monitors.length; ++i) {
+		    if (Main.layoutManager.monitors[i] != Main.layoutManager.primaryMonitor) {
+			monitors.push(Main.layoutManager.monitors[i]);
+		    }
+		}
+	    } else {
+		log("_showOverlays(): Unhandled \"monitors\" setting = "+monitorConfig);
+		return;
+	    }
+	    this._overlays = [];
+	    for (let i=0; i < monitors.length; ++i) {
+		let monitor = monitors[i];
+		log_debug('Create overlay #'+i+': '+monitor.width+'x'+monitor.height+'@'+monitor.x+','+monitor.y);
+		let overlay = new St.Label({
+		    style_class: 'brightness-overlay',
+		    text: "",
+		});
+		overlay.set_position(monitor.x, monitor.y);
+		overlay.set_width(monitor.width);
+		overlay.set_height(monitor.height);
+		overlay.set_z_position(0.00001);
+		Main.uiGroup.add_actor(overlay);
+		this._overlays.push(overlay);
+	    }
+	}
+
+	for (let i=0; i < this._overlays.length; ++i) {
+	    log_debug('_showOverlay(): set opacity '+opacity+' on overlay #'+i);
+	    this._overlays[i].opacity = opacity;
+	}
+    },
+
+    _storeBrightnessLevel(value) {
+	if (settings.get_boolean('use-backlight') && this._proxy.Brightness >= 0) {
+	    let convertedBrightness = Math.min(100, Math.round(value * 100.0)+1);
+	    log_debug('_storeBrightnessLevel('+value+') by proxy -> '+convertedBrightness);
+	    this._proxy.Brightness = convertedBrightness;
+	} else {
+	    log_debug('_storeBrightnessLevel('+value+') by setting');
+	    settings.set_double('current-brightness', value);
+	}
+    },
+
+    _getBrightnessLevel() {
+	let brightness = this._proxy.Brightness;
+	if (settings.get_boolean('use-backlight') && brightness != brightness >= 0) {
+	    let convertedBrightness = brightness / 100.0;
+	    log_debug('_getBrightnessLevel() by proxy = '+convertedBrightness+' <- '+brightness);
+	    return convertedBrightness;
+	} else {
+	    brightness = settings.get_double('current-brightness');
+	    log_debug('_getBrightnessLevel() by setting = '+brightness);
+	    return brightness;
+	}
+    },
+
+    _on_brightness_change() {
+	let curBrightness = this._getBrightnessLevel();
+	let minBrightness = settings.get_double('min-brightness');
+
+	log_debug("_on_brightness_change: current-brightness="+curBrightness+", min-brightness="+minBrightness);
+	if (curBrightness < minBrightness) {
+	    curBrightness = minBrightness;
+	    if (! settings.get_boolean('use-backlight')) {
+		this._slider.setValue(curBrightness);
+	    }
+	    this._storeBrightnessLevel(minBrightness);
+	    return;
+	}
+	if (curBrightness >= 1) {
+	    this._hideOverlays();
+	} else {
+	    let opacity = (1-curBrightness)*255;
+	    log_debug("_on_brightness_change: opacity="+opacity);
+	    this._showOverlays(opacity);
+	}
+    },
+
+    _on_monitors_change() {
+	log_debug('_on_monitors_change()');
+	this._hideOverlays();
+	this._on_brightness_change();
+    },
+
+    _on_use_backlight_change() {
+	log_debug('_on_use_backlight_change()');
+	if (settings.get_boolean('use-backlight')) {
+	    this._storeBrightnessLevel(settings.get_double('current-brightness'));
+	} else if (this._proxy.Brightness != null && this._proxy.Brightness >= 0) {
+	    this._storeBrightnessLevel(this._proxy.Brightness / 100.0);
 	}
     }
 
-    for (let i=0; i < overlays.length; ++i) {
-	overlays[i].opacity = opacity;
-    }
-}
-
-function storeBrightnessLevel(value) {
-    if (settings.get_boolean('use-backlight') && Brightness._proxy.Brightness >= 0) {
-	let convertedBrightness = Math.min(100, Math.round(value * 100.0)+1);
-	log_debug('storeBrightnessLevel('+value+') by proxy -> '+convertedBrightness);
-	Brightness._proxy.Brightness = convertedBrightness;
-    } else {
-	log_debug('storeBrightnessLevel('+value+') by setting');
-	settings.set_double('current-brightness', value);
-    }
-}
-
-function getBrightnessLevel() {
-    let brightness = Brightness._proxy.Brightness;
-    if (settings.get_boolean('use-backlight') && brightness != brightness >= 0) {
-	let convertedBrightness = brightness / 100.0;
-	log_debug('getBrightnessLevel() by proxy = '+convertedBrightness+' <- '+brightness);
-	return convertedBrightness;
-    } else {
-	brightness = settings.get_double('current-brightness');
-	log_debug('getBrightnessLevel() by setting = '+brightness);
-	return brightness;
-    }
-}
-
-function sliderChanged(slider, value) {
-    log_debug("sliderChanged, value="+value);
-    storeBrightnessLevel(value);
-}
+});
 
 function on_debug_change() {
     debug = settings.get_boolean('debug');
     log('debug = '+debug);
-}
-
-function on_brightness_change() {
-    let curBrightness = getBrightnessLevel();
-    let minBrightness = settings.get_double('min-brightness');
-
-    log_debug("on_brightness_change: current-brightness="+curBrightness+", min-brightness="+minBrightness);
-    if (curBrightness < minBrightness) {
-	curBrightness = minBrightness;
-	if (! settings.get_boolean('use-backlight')) {
-	    BrightnessSlider.setValue(curBrightness);
-	}
-	storeBrightnessLevel(minBrightness);
-	return;
-    }
-    if (curBrightness >= 1) {
-	hideOverlays();
-    } else {
-	let opacity = (1-curBrightness)*255;
-	log_debug("on_current_brightness_change: opacity="+opacity);
-	showOverlays(opacity);
-    }
-}
-
-function on_monitors_change() {
-    log_debug('on_monitors_change()');
-    hideOverlays();
-    on_brightness_change();
-}
-
-function replacement_sync() {
-    log_debug('Brightness Indicator _sync()');
-    on_brightness_change();
-    BrightnessSlider.setValue(getBrightnessLevel());
-}
-
-function on_use_backlight_change() {
-    log_debug('on_use_backlight_change()');
-    if (settings.get_boolean('use-backlight')) {
-	storeBrightnessLevel(settings.get_double('current-brightness'));
-    } else if (Brightness._proxy.Brightness != null && Brightness._proxy.Brightness >= 0) {
-	storeBrightnessLevel(Brightness._proxy.Brightness / 100.0);
-    }
 }
 
 function init() {
@@ -188,52 +255,15 @@ function enable() {
     if (enabled) {
 	log_debug('enable(), session mode = '+Main.sessionMode.currentMode+", skipping as already enabled");
     } else {
-	if (sliderChangedBacklightCode == null) {
-	    for (let i= BrightnessSlider._signalConnections.length-1; i>=0; --i) {
-		let signalConnection = BrightnessSlider._signalConnections[i];
-		if (signalConnection.name == 'value-changed') {
-		    sliderChangedBacklightCode = signalConnection.callback;
-		    sliderChangedBacklightConnection = signalConnection.id;
-		    log_debug("Identified signal id "+sliderChangedBacklightConnection+" as backlight brightness control");
-		    break;
-		}
-	    }
-	    if (sliderChangedBacklightCode == null) {
-		log("cannot find brightness value-changed handler");
-		return;
-	    }
-	}
 	settings = Convenience.getSettings();
 	debugSettingChangedConnection = settings.connect('changed::debug', on_debug_change);
 	debug = settings.get_boolean('debug');
 	log_debug('enable(), session mode = '+Main.sessionMode.currentMode);
-	if (sliderChangedBacklightConnection != null) {
-	    BrightnessSlider.disconnect(sliderChangedBacklightConnection);
-	    sliderChangedBacklightConnection = null;
-	}
-	if (brightnessIndicatorOriginalSync == null) {
-	    brightnessIndicatorOriginalSync = imports.ui.status.brightness.Indicator.prototype._sync;
-	    imports.ui.status.brightness.Indicator.prototype._sync = imports.ui.status.brightness.Indicator.wrapFunction('_sync', replacement_sync);
-	}
-	sliderChangedConnection = BrightnessSlider.connect('value-changed', sliderChanged);
-	monitorsChangedConnection = Main.layoutManager.connect('monitors-changed', on_monitors_change);
-	minBrightnessSettingChangedConnection = settings.connect('changed::min-brightness', on_brightness_change);
-	currentBrightnessSettingChangedConnection = settings.connect('changed::current-brightness', on_brightness_change);
-	monitorsSettingChangedConnection = settings.connect('changed::monitors', on_monitors_change);
-	useBacklightSettingChangedConnection = settings.connect('changed::use-backlight', on_use_backlight_change);
-
-	// If we use the backlight and the Brightness proxy is null, it's still connecting and we'll get a _sync later.
-	if (! settings.get_boolean('use-backlight') || Brightness._proxy.Brightness != null) {
-	    let curBrightness = getBrightnessLevel();
-	    sliderChanged(BrightnessSlider, curBrightness);
-	    BrightnessSlider.setValue(curBrightness);
-	}
-
-	// Show the slider, it can be hidden on some systems - this is
-	// not undone at disable() time
-	Brightness._item.actor.visible = true;
+	modifiedIndicator = new ModifiedIndicator();
+	modifiedIndicator._enable();
 
 	enabled = true;
+	log_debug('Extension enabled');
     }
 }
 
@@ -243,21 +273,11 @@ function disable() {
     } else if (enabled) {
 	log_debug('disable(), session mode = '+Main.sessionMode.currentMode);
 	settings.disconnect(debugSettingChangedConnection);
-	Main.layoutManager.disconnect(monitorsChangedConnection);
-	settings.disconnect(minBrightnessSettingChangedConnection);
-	settings.disconnect(currentBrightnessSettingChangedConnection);
-	settings.disconnect(monitorsSettingChangedConnection);
-	BrightnessSlider.disconnect(sliderChangedConnection);
-	if (sliderChangedBacklightConnection == null) {
-	    sliderChangedBacklightConnection = BrightnessSlider.connect('value-changed', sliderChangedBacklightCode);
-	}
-	if (brightnessIndicatorOriginalSync != null) {
-	    imports.ui.status.brightness.Indicator.prototype._sync = brightnessIndicatorOriginalSync;
-	    brightnessIndicatorOriginalSync = null;
-	}
-	hideOverlays();
+	modifiedIndicator._disable();
+	modifiedIndicator = null;
 	settings.run_dispose();
 	enabled = false;
+	log_debug('Extension disabled');
     } else {
 	log('disabled() called when not enabled');
     }
