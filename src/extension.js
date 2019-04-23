F-i-f/soft-brightness

commit 69b001536b9270044fc8f49f8ecf1c4c8c12c2bd

Fix typo & bug.
This typo was causing a flash of brightness when the brightness slider
was adjusted close to zero brightness with the use-backlight setting
to on.

src/extension.js
@@ -395,13 +395,13 @@ const SoftBrightnessExtension = class SoftBrightnessExtension {
     }
 
     _getBrightnessLevel() {
-	let brightness = this._brightnessIndicator._proxy.Brightness;
-	if (this._settings.get_boolean('use-backlight') && brightness != brightness >= 0) {
+	if (this._settings.get_boolean('use-backlight')) {
+	    let brightness = this._brightnessIndicator._proxy.Brightness;
 	    let convertedBrightness = brightness / 100.0;
 	    this._logger.log_debug('_getBrightnessLevel() by proxy = '+convertedBrightness+' <- '+brightness);
 	    return convertedBrightness;
 	} else {
-	    brightness = this._settings.get_double('current-brightness');
+	    let brightness = this._settings.get_double('current-brightness');
 	    this._logger.log_debug('_getBrightnessLevel() by setting = '+brightness);
 	    return brightness;
 	}

commit 14c18b01daaa15ad71b9eb1d51a6b7a4c4fd8b17

Fix a regression when using "Use backlight" w/o backlight
Regression introduced in 69b001536b9270044fc8f49f8ecf1c4c8c12c2bd.
When attempting to use the backlight, if there is no backend connected
the proxy will return -1.  Only use the proxy if it returns a positive
number.

src/extension.js
@@ -381,13 +381,13 @@ const SoftBrightnessExtension = class SoftBrightnessExtension {
     }
 
     _getBrightnessLevel() {
-	if (this._settings.get_boolean('use-backlight')) {
-	    let brightness = this._brightnessIndicator._proxy.Brightness;
+	let brightness = this._brightnessIndicator._proxy.Brightness;
+	if (this._settings.get_boolean('use-backlight') && brightness >= 0) {
 	    let convertedBrightness = brightness / 100.0;
 	    this._logger.log_debug('_getBrightnessLevel() by proxy = '+convertedBrightness+' <- '+brightness);
 	    return convertedBrightness;
 	} else {
-	    let brightness = this._settings.get_double('current-brightness');
+	    brightness = this._settings.get_double('current-brightness');
 	    this._logger.log_debug('_getBrightnessLevel() by setting = '+brightness);
 	    return brightness;
 	}