705e464f93d02cdeb0baa23dc572db258ee113e4
Philippe Troin <phil+github-commits@fifi.org>
2020-03-11 17:33:07 -0700

    Update margin_left/right props to start/end.
    GtkWidget margin_left and margin_right properties are deprecated.

--- src/prefs.js
+++ src/prefs.js
@@ -34,8 +34,8 @@ const SoftBrightnessSettings = GObject.registerClass(class SoftBrightnessSetting
     setup() {
 	this.margin_top = 12;
 	this.margin_bottom = this.margin_top;
-	this.margin_left = 48;
-	this.margin_right = this.margin_left;
+	this.margin_start = 48;
+	this.margin_end = this.margin_start;
 	this.row_spacing = 6;
 	this.column_spacing = this.row_spacing;
 	this.orientation = Gtk.Orientation.VERTICAL;
